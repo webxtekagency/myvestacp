b2ddf443b8e21613ee175c8359452ada3a5ecb0e
myvesta <38690722+myvesta@users.noreply.github.com>
2021-02-21 23:02:17 +0100

    Support for php8.0

--- src/deb/for-download/tools/multi-php-install.sh
+++ src/deb/for-download/tools/multi-php-install.sh
@@ -10,6 +10,7 @@ inst_71=0
 inst_72=0
 inst_73=0
 inst_74=0
+inst_80=0
 
 #######################################################################
 
@@ -43,8 +44,11 @@ fi
 if [ $# -gt 6 ]; then
     inst_74=$7
 fi
+if [ $# -gt 7 ]; then
+    inst_80=$8
+fi
 
-if [ $inst_56 -eq 1 ] || [ $inst_70 -eq 1 ] || [ $inst_71 -eq 1 ] || [ $inst_72 -eq 1 ] || [ $inst_73 -eq 1 ] || [ $inst_74 -eq 1 ]; then
+if [ $inst_56 -eq 1 ] || [ $inst_70 -eq 1 ] || [ $inst_71 -eq 1 ] || [ $inst_72 -eq 1 ] || [ $inst_73 -eq 1 ] || [ $inst_74 -eq 1 ] || [ $inst_80 -eq 1 ]; then
     inst_repo=1
 fi
 
@@ -71,27 +75,28 @@ echo "inst_71=$inst_71"
 echo "inst_72=$inst_72"
 echo "inst_73=$inst_73"
 echo "inst_74=$inst_74"
+echo "inst_80=$inst_80"
 echo "wait_to_press_enter=$wait_to_press_enter"
 
 press_enter "=== Press enter to continue ==============================================================================="
 
 apt update
 if [ "$inst_repo" -eq 1 ]; then
-press_enter "=== Press enter to install sury.org repo ==============================================================================="
-apt -y install apt-transport-https ca-certificates
-wget -nv -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-if [ $debian_version -eq 8 ]; then
-  sh -c 'echo "deb https://packages.sury.org/php/ jessie main" > /etc/apt/sources.list.d/php.list'
-fi
-if [ $debian_version -eq 9 ]; then
-  sh -c 'echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/php.list'
-fi
-if [ $debian_version -eq 10 ]; then
-  sh -c 'echo "deb https://packages.sury.org/php/ buster main" > /etc/apt/sources.list.d/php.list'
-fi
-apt update
-apt upgrade -y
-press_enter "=== Press enter to continue ==============================================================================="
+    press_enter "=== Press enter to install sury.org repo ==============================================================================="
+    apt -y install apt-transport-https ca-certificates
+    wget -nv -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+    if [ $debian_version -eq 8 ]; then
+      sh -c 'echo "deb https://packages.sury.org/php/ jessie main" > /etc/apt/sources.list.d/php.list'
+    fi
+    if [ $debian_version -eq 9 ]; then
+      sh -c 'echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/php.list'
+    fi
+    if [ $debian_version -eq 10 ]; then
+      sh -c 'echo "deb https://packages.sury.org/php/ buster main" > /etc/apt/sources.list.d/php.list'
+    fi
+    apt update
+    apt upgrade -y
+    press_enter "=== Press enter to continue ==============================================================================="
 fi
 
 echo "=== Enabling proxy_fcgi setenvif"
@@ -100,107 +105,107 @@ service apache2 restart
 
 
 if [ "$inst_56" -eq 1 ]; then
-press_enter "=== Press enter to install PHP 5.6 ==============================================================================="
-apt -y install php5.6-mbstring php5.6-bcmath php5.6-cli php5.6-curl php5.6-fpm php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mysql php5.6-soap php5.6-xml php5.6-zip php5.6-memcache php5.6-memcached
-update-rc.d php5.6-fpm defaults
-a2enconf php5.6-fpm
-systemctl restart apache2
-cp -r /etc/php/5.6/ /root/vst_install_backups/php5.6/
-# rm -f /etc/php/5.6/fpm/pool.d/*
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-56.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-56.stpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-56.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-56.tpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-56.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-56.sh
-chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-56.sh
-mkdir -p /root/vesta-temp-dl/vesta/patch
-wget -nv https://c.myvestacp.com/tools/patches/php5.6.patch -O /root/vesta-temp-dl/vesta/patch/php5.6.patch
-patch -p1 --directory=/ < /root/vesta-temp-dl/vesta/patch/php5.6.patch
-rm -rf /root/vesta-temp-dl
-press_enter "=== Press enter to continue ==============================================================================="
+    press_enter "=== Press enter to install PHP 5.6 ==============================================================================="
+    apt -y install php5.6-mbstring php5.6-bcmath php5.6-cli php5.6-curl php5.6-fpm php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mysql php5.6-soap php5.6-xml php5.6-zip php5.6-memcache php5.6-memcached
+    update-rc.d php5.6-fpm defaults
+    a2enconf php5.6-fpm
+    systemctl restart apache2
+    cp -r /etc/php/5.6/ /root/vst_install_backups/php5.6/
+    # rm -f /etc/php/5.6/fpm/pool.d/*
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-56.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-56.stpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-56.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-56.tpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-56.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-56.sh
+    chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-56.sh
+    mkdir -p /root/vesta-temp-dl/vesta/patch
+    wget -nv https://c.myvestacp.com/tools/patches/php5.6.patch -O /root/vesta-temp-dl/vesta/patch/php5.6.patch
+    patch -p1 --directory=/ < /root/vesta-temp-dl/vesta/patch/php5.6.patch
+    rm -rf /root/vesta-temp-dl
+    press_enter "=== Press enter to continue ==============================================================================="
 fi
 
 if [ "$inst_70" -eq 1 ]; then
-press_enter "=== Press enter to install PHP 7.0 ==============================================================================="
-apt -y install php7.0-mbstring php7.0-bcmath php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-intl php7.0-mcrypt php7.0-mysql php7.0-soap php7.0-xml php7.0-zip php7.0-memcache php7.0-memcached
-update-rc.d php7.0-fpm defaults
-a2enconf php7.0-fpm
-systemctl restart apache2
-cp -r /etc/php/7.0/ /root/vst_install_backups/php7.0/
-# rm -f /etc/php/7.0/fpm/pool.d/*
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-70.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70.stpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-70.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70.tpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-70.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70.sh
-chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70.sh
-if [ $debian_version -eq 9 ]; then
-  cp /etc/php/7.0/apache2/php.ini /etc/php/7.0/fpm/php.ini
-fi
-if [ $debian_version -eq 10 ]; then
-  cp /etc/php/7.3/fpm/php.ini /etc/php/7.0/fpm/php.ini
-fi
-press_enter "=== Press enter to continue ==============================================================================="
+    press_enter "=== Press enter to install PHP 7.0 ==============================================================================="
+    apt -y install php7.0-mbstring php7.0-bcmath php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-intl php7.0-mcrypt php7.0-mysql php7.0-soap php7.0-xml php7.0-zip php7.0-memcache php7.0-memcached
+    update-rc.d php7.0-fpm defaults
+    a2enconf php7.0-fpm
+    systemctl restart apache2
+    cp -r /etc/php/7.0/ /root/vst_install_backups/php7.0/
+    # rm -f /etc/php/7.0/fpm/pool.d/*
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-70.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70.stpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-70.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70.tpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-70.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70.sh
+    chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70.sh
+    if [ $debian_version -eq 9 ]; then
+      cp /etc/php/7.0/apache2/php.ini /etc/php/7.0/fpm/php.ini
+    fi
+    if [ $debian_version -eq 10 ]; then
+      cp /etc/php/7.3/fpm/php.ini /etc/php/7.0/fpm/php.ini
+    fi
+    press_enter "=== Press enter to continue ==============================================================================="
 fi
 
 if [ "$inst_71" -eq 1 ]; then
-press_enter "=== Press enter to install PHP 7.1 ==============================================================================="
-apt -y install php7.1-mbstring php7.1-bcmath php7.1-cli php7.1-curl php7.1-fpm php7.1-gd php7.1-intl php7.1-mcrypt php7.1-mysql php7.1-soap php7.1-xml php7.1-zip php7.1-memcache php7.1-memcached
-update-rc.d php7.1-fpm defaults
-a2enconf php7.1-fpm
-systemctl restart apache2
-cp -r /etc/php/7.1/ /root/vst_install_backups/php7.1/
-# rm -f /etc/php/7.1/fpm/pool.d/*
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-71.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71.stpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-71.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71.tpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-71.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71.sh
-chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71.sh
-if [ $debian_version -eq 9 ]; then
-  cp /etc/php/7.0/apache2/php.ini /etc/php/7.1/fpm/php.ini
-fi
-if [ $debian_version -eq 10 ]; then
-  cp /etc/php/7.3/fpm/php.ini /etc/php/7.1/fpm/php.ini
-fi
-press_enter "=== Press enter to continue ==============================================================================="
+    press_enter "=== Press enter to install PHP 7.1 ==============================================================================="
+    apt -y install php7.1-mbstring php7.1-bcmath php7.1-cli php7.1-curl php7.1-fpm php7.1-gd php7.1-intl php7.1-mcrypt php7.1-mysql php7.1-soap php7.1-xml php7.1-zip php7.1-memcache php7.1-memcached
+    update-rc.d php7.1-fpm defaults
+    a2enconf php7.1-fpm
+    systemctl restart apache2
+    cp -r /etc/php/7.1/ /root/vst_install_backups/php7.1/
+    # rm -f /etc/php/7.1/fpm/pool.d/*
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-71.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71.stpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-71.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71.tpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-71.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71.sh
+    chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71.sh
+    if [ $debian_version -eq 9 ]; then
+      cp /etc/php/7.0/apache2/php.ini /etc/php/7.1/fpm/php.ini
+    fi
+    if [ $debian_version -eq 10 ]; then
+      cp /etc/php/7.3/fpm/php.ini /etc/php/7.1/fpm/php.ini
+    fi
+    press_enter "=== Press enter to continue ==============================================================================="
 fi
 
 if [ "$inst_72" -eq 1 ]; then
-press_enter "=== Press enter to install PHP 7.2 ==============================================================================="
-apt -y install php7.2-mbstring php7.2-bcmath php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-intl php7.2-mysql php7.2-soap php7.2-xml php7.2-zip php7.2-memcache php7.2-memcached
-update-rc.d php7.2-fpm defaults
-a2enconf php7.2-fpm
-systemctl restart apache2
-cp -r /etc/php/7.2/ /root/vst_install_backups/php7.2/
-# rm -f /etc/php/7.2/fpm/pool.d/*
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-72.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72.stpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-72.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72.tpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-72.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72.sh
-chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72.sh
-if [ $debian_version -eq 9 ]; then
-  cp /etc/php/7.0/apache2/php.ini /etc/php/7.2/fpm/php.ini
-fi
-if [ $debian_version -eq 10 ]; then
-  cp /etc/php/7.3/fpm/php.ini /etc/php/7.2/fpm/php.ini
-fi
-press_enter "=== Press enter to continue ==============================================================================="
+    press_enter "=== Press enter to install PHP 7.2 ==============================================================================="
+    apt -y install php7.2-mbstring php7.2-bcmath php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-intl php7.2-mysql php7.2-soap php7.2-xml php7.2-zip php7.2-memcache php7.2-memcached
+    update-rc.d php7.2-fpm defaults
+    a2enconf php7.2-fpm
+    systemctl restart apache2
+    cp -r /etc/php/7.2/ /root/vst_install_backups/php7.2/
+    # rm -f /etc/php/7.2/fpm/pool.d/*
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-72.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72.stpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-72.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72.tpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-72.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72.sh
+    chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72.sh
+    if [ $debian_version -eq 9 ]; then
+      cp /etc/php/7.0/apache2/php.ini /etc/php/7.2/fpm/php.ini
+    fi
+    if [ $debian_version -eq 10 ]; then
+      cp /etc/php/7.3/fpm/php.ini /etc/php/7.2/fpm/php.ini
+    fi
+    press_enter "=== Press enter to continue ==============================================================================="
 fi
 
 if [ "$inst_73" -eq 1 ]; then
-press_enter "=== Press enter to install PHP 7.3 ==============================================================================="
-apt -y install php7.3-mbstring php7.3-bcmath php7.3-cli php7.3-curl php7.3-fpm php7.3-gd php7.3-intl php7.3-mysql php7.3-soap php7.3-xml php7.3-zip php7.3-memcache php7.3-memcached
-update-rc.d php7.3-fpm defaults
-a2enconf php7.3-fpm
-systemctl restart apache2
-cp -r /etc/php/7.3/ /root/vst_install_backups/php7.3/
-# rm -f /etc/php/7.3/fpm/pool.d/*
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73.stpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73.tpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73.sh
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73-public.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-public.stpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73-public.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-public.tpl
-wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73-public.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-public.sh
-chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73.sh
-chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-public.sh
-if [ $debian_version -eq 9 ]; then
-  cp /etc/php/7.0/apache2/php.ini /etc/php/7.3/fpm/php.ini
-fi
-press_enter "=== Press enter to continue ==============================================================================="
+    press_enter "=== Press enter to install PHP 7.3 ==============================================================================="
+    apt -y install php7.3-mbstring php7.3-bcmath php7.3-cli php7.3-curl php7.3-fpm php7.3-gd php7.3-intl php7.3-mysql php7.3-soap php7.3-xml php7.3-zip php7.3-memcache php7.3-memcached
+    update-rc.d php7.3-fpm defaults
+    a2enconf php7.3-fpm
+    systemctl restart apache2
+    cp -r /etc/php/7.3/ /root/vst_install_backups/php7.3/
+    # rm -f /etc/php/7.3/fpm/pool.d/*
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73.stpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73.tpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73.sh
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73-public.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-public.stpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73-public.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-public.tpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-73-public.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-public.sh
+    chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73.sh
+    chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-public.sh
+    if [ $debian_version -eq 9 ]; then
+      cp /etc/php/7.0/apache2/php.ini /etc/php/7.3/fpm/php.ini
+    fi
+    press_enter "=== Press enter to continue ==============================================================================="
 fi
 
 if [ "$inst_74" -eq 1 ]; then
@@ -228,6 +233,28 @@ if [ "$inst_74" -eq 1 ]; then
     press_enter "=== Press enter to continue ==============================================================================="
 fi
 
+
+if [ "$inst_80" -eq 1 ]; then
+    press_enter "=== Press enter to install PHP 8.0 ==============================================================================="
+    apt -y install php8.0-mbstring php8.0-bcmath php8.0-cli php8.0-curl php8.0-fpm php8.0-gd php8.0-intl php8.0-mysql php8.0-soap php8.0-xml php8.0-zip php8.0-memcache php8.0-memcached
+    update-rc.d php8.0-fpm defaults
+    a2enconf php8.0-fpm
+    a2dismod php8.0
+    apt-get -y remove libapache2-mod-php8.0
+    systemctl restart apache2
+    cp -r /etc/php/8.0/ /root/vst_install_backups/php8.0/
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-80.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80.stpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-80.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80.tpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-80.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80.sh
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-80-public.stpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80-public.stpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-80-public.tpl -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80-public.tpl
+    wget -nv https://c.myvestacp.com/tools/apache-fpm-tpl/PHP-FPM-80-public.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80-public.sh
+    chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80.sh
+    chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80-public.sh
+    press_enter "=== Press enter to continue ==============================================================================="
+fi
+
+
 apt update
 apt upgrade -y
 
@@ -243,5 +270,6 @@ if [ $debian_version -eq 10 ]; then
     a2dismod php8.0
     a2dismod mpm_prefork
     a2enmod mpm_event
+    apt-get -y remove libapache2-mod-php8.0
     service apache2 restart
 fi

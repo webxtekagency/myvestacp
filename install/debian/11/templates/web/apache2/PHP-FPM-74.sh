4942f52f7feb85edde00d588c4f340ceaa1bf551
myvesta <38690722+myvesta@users.noreply.github.com>
2021-08-24 16:46:40 +0200

    systemctl reset-failed php7.4-fpm

--- install/debian/11/templates/web/apache2/PHP-FPM-74.sh
+++ install/debian/11/templates/web/apache2/PHP-FPM-74.sh
@@ -17,8 +17,8 @@ user = $1
 group = $1
 
 pm = ondemand
-pm.max_children = 8
-request_terminate_timeout = 90s
+pm.max_children = 16
+request_terminate_timeout = 30s
 pm.max_requests = 4000
 pm.process_idle_timeout = 10s
 pm.status_path = /status
@@ -27,7 +27,7 @@ php_admin_value[upload_tmp_dir] = /home/$1/tmp
 php_admin_value[session.save_path] = /home/$1/tmp
 php_admin_value[open_basedir] = $5:/home/$1/tmp:/bin:/usr/bin:/usr/local/bin:/var/www/html:/tmp:/usr/share:/etc/phpmyadmin:/var/lib/phpmyadmin:/etc/roundcube:/var/log/roundcube:/var/lib/roundcube
 php_admin_value[upload_max_filesize] = 80M
-php_admin_value[max_execution_time] = 30
+php_admin_value[max_execution_time] = 20
 php_admin_value[post_max_size] = 80M
 php_admin_value[memory_limit] = 256M
 php_admin_value[sendmail_path] = \"/usr/sbin/sendmail -t -i -f info@$2\"
@@ -46,29 +46,35 @@ pool_file_71="/etc/php/7.1/fpm/pool.d/$2.conf"
 pool_file_72="/etc/php/7.2/fpm/pool.d/$2.conf"
 pool_file_73="/etc/php/7.3/fpm/pool.d/$2.conf"
 pool_file_74="/etc/php/7.4/fpm/pool.d/$2.conf"
+pool_file_80="/etc/php/8.0/fpm/pool.d/$2.conf"
 
 if [ -f "$pool_file_56" ]; then
     rm $pool_file_56
+    systemctl reset-failed php5.6-fpm
     service php5.6-fpm restart
 fi
 
 if [ -f "$pool_file_70" ]; then
     rm $pool_file_70
+    systemctl reset-failed php7.0-fpm
     service php7.0-fpm restart
 fi
 
 if [ -f "$pool_file_71" ]; then
     rm $pool_file_71
+    systemctl reset-failed php7.1-fpm
     service php7.1-fpm restart
 fi
 
 if [ -f "$pool_file_72" ]; then
     rm $pool_file_72
+    systemctl reset-failed php7.2-fpm
     service php7.2-fpm restart
 fi
 
 if [ -f "$pool_file_73" ]; then
     rm $pool_file_73
+    systemctl reset-failed php7.3-fpm
     service php7.3-fpm restart
 fi
 
@@ -83,10 +89,17 @@ else
 fi
 if [ $write_file -eq 1 ]; then
     echo "$pool_conf" > $pool_file_74
+    systemctl reset-failed php7.4-fpm
     service php7.4-fpm restart
 fi
 if [ -f "/etc/php/7.4/fpm/pool.d/www.conf" ]; then
     rm /etc/php/7.4/fpm/pool.d/www.conf
+fi
+
+if [ -f "$pool_file_80" ]; then
+    rm $pool_file_80
+    systemctl reset-failed php8.0-fpm
+    service php8.0-fpm restart
 fi
 
 exit 0

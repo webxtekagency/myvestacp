c2a30bcd31d41ce90146734af3d52073ae1c169a
myvesta <38690722+myvesta@users.noreply.github.com>
2021-08-29 00:07:41 +0200

    Preventing CSRF in /edit/file/

--- web/edit/file/index.php
+++ web/edit/file/index.php
@@ -1,15 +1,7 @@
 <?php
 
 // Preventing CSRF
-if ($_SERVER['REQUEST_METHOD']=='POST') {
-    $host_arr=explode(":", $_SERVER['HTTP_HOST']);
-    $hostname=$host_arr[0];
-    $port = $_SERVER['SERVER_PORT'];
-    $expected_http_origin="https://".$hostname.":".$port;
-    if ($_SERVER['HTTP_ORIGIN'] != $expected_http_origin) {
-        die ("Nope.");
-    }
-}
+prevent_post_csrf(true);
 
 include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
 $user = $_SESSION['user'];
